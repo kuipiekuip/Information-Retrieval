32c38c339399a492bfb30eb2a3df01c8a5d6c738
Martijn Smits <martijn.smits@outlook.com>
2024-03-27 14:25:20 +0100

    evaluation wip

--- src/evaluation.py
+++ src/evaluation.py
@@ -6,17 +6,20 @@ def evaluation(dataset_name : str):
     dataset = pt.get_dataset(dataset_name)
     index = dataset.get_index(variant="terrier_stemmed")
 
-    # tfidf = pt.BatchRetrieve(index, wmodel="TF_IDF")
+    tfidf = pt.BatchRetrieve(index, wmodel="TF_IDF")
     bm25 = pt.BatchRetrieve(index, wmodel="BM25")
 
-    pt.Experiment(
-        [bm25],
+    test = pt.Experiment(
+        [tfidf, bm25],
         dataset.get_topics(),
         dataset.get_qrels(),
         eval_metrics=[RR @ 10, nDCG @ 20, MAP],
+        names=["TF_IDF", "BM25"],
+        baseline=0
     )
+    print(test)
 
 if not pt.started():
     pt.init()
-    
+
 evaluation("vaswani")
