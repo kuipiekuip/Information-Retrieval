4df3633ba108044b7477b0251491935f3c2fc0f7
Martijn Smits <martijn.smits@outlook.com>
2024-03-29 13:55:23 +0100

    evaluation nyt dataset

--- src/evaluation.py
+++ src/evaluation.py
@@ -2,28 +2,28 @@ import pyterrier as pt
 import pandas as pd
 from pyterrier.measures import RR, nDCG, MAP
 
-def run_evaluation(dataset):
-    # dataset = pt.get_dataset(dataset_name) 
+def run_evaluation():
+    dataset = pt.datasets.get_dataset('irds:nyt/trec-core-2017')
+    queries = dataset.get_topics()
+    queries.rename(columns={'title': 'query'}, inplace=True)
+    print(queries.dtypes)
+    index_ref = pt.IndexRef.of('./indices/nyt')
 
-    default_queries = dataset.get_topics()
+    # dummy_df = default_queries.copy() # This is where we read in our own query expansions
+    # dummy_df['query'] = 'some query'
 
-    dummy_df = default_queries.copy() # This is where we read in our own query expansions
-    dummy_df['query'] = 'some query'
+    # expanded_regular_queries = dummy_df
+    # expanded_news_queries = dummy_df
 
-    expanded_regular_queries = dummy_df
-    expanded_news_queries = dummy_df
+    result_default = evaluation(dataset, index_ref, queries)
+    # result_expanded_regular = evaluation(dataset, expanded_regular_queries)
+    # result_expanded_news = evaluation(dataset, expanded_news_queries)
 
-    result_default = evaluation(dataset, default_queries)
-    result_expanded_regular = evaluation(dataset, expanded_regular_queries)
-    result_expanded_news = evaluation(dataset, expanded_news_queries)
+    return result_default#, result_expanded_regular, result_expanded_news
 
-    return result_default, result_expanded_regular, result_expanded_news
-
-def evaluation(dataset, topics : pd.DataFrame) -> pd.DataFrame:
-    index = dataset.get_index(variant="terrier_stemmed")
-
-    tfidf = pt.BatchRetrieve(index, wmodel="TF_IDF")
-    bm25 = pt.BatchRetrieve(index, wmodel="BM25")
+def evaluation(dataset, index_ref, topics : pd.DataFrame) -> pd.DataFrame:
+    tfidf = pt.BatchRetrieve(index_ref, wmodel="TF_IDF")
+    bm25 = pt.BatchRetrieve(index_ref, wmodel="BM25")
 
     return pt.Experiment(
         [tfidf, bm25],
@@ -35,30 +35,6 @@ def evaluation(dataset, topics : pd.DataFrame) -> pd.DataFrame:
 if not pt.started():
     pt.init()
 
-# docs = load_dataset('irds/nyt', 'docs')
-# for record in docs:
-#         print(record)
 
-
-# dataset = pt.datasets.get_dataset('irds:cord19/fulltext/trec-covid')
-# print(dataset.get_topics())
-# print(dataset.get_qrels())
-# for document in dataset.get_corpus_iter():
-    # print(document)
-
-
-dataset = pt.datasets.get_dataset('irds:nyt/trec-core-2017')
-# print(dataset.get_topics())
-# print(dataset.get_qrels())
-for document in dataset.get_corpus_iter():
-    print(document)
-    
-
-default, regular, news = run_evaluation(dataset)
-print(default)
-print(regular)
-print(news)
-
-# 3 queries, 1-3 datasets, 1-3 weighting models
-
-# DataFrame['qid','query] -> for each query we find all urls -> for all urls we retrieve the text -> we do the magic -> create expanded -> DataFrrame['qid','query']
+result = run_evaluation()
+print(result)
