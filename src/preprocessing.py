8a840802ef937441f8417286d7486857dd3dd8cb
Martijn Smits <martijn.smits@outlook.com>
2024-03-29 15:30:20 +0100

    fix

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,23 +4,23 @@ import re
 from typing import List
 
 
-def download_all_documents(url_list : List[str]) -> List[requests.models.Response]:
+def download_all_documents(url_list : List[str]) -> List[str]:
     return list(map(download_document, url_list))
 
 def download_document(url : str) -> requests.models.Response:
-    response = ""
     try:
         response = requests.get(url)
     except:
         print("Exception: Connection Problem; skipping file")
+        return ""
 
-    return response
+    return response.content
 
 def clean_all_documents(page_list : List[requests.models.Response]):
     return list(map(clean_document, page_list))
 
-def clean_document(page : requests.models.Response):
-    soup = BeautifulSoup(page.content, "html.parser")
+def clean_document(content : str):
+    soup = BeautifulSoup(content, "html.parser")
     
     body_text = ""
     for tag in soup.find_all(['h1','p']):
@@ -31,7 +31,7 @@ def remove_punctuation(text : str) -> str:
     return str(re.sub(r'[^\s\w](?![\w])|(?<![\w])[^\s\w]|^[^\s\w]|[^\s\w]$|\n|\xa0|\[|\]', '', text))
 
 if __name__== "__main__" :
-
+    clean_all_documents(["", "test"])
     url_list = ["https://nos.nl/artikel/2513343-unilever-schrapt-7500-banen-en-zet-ijsjes-in-de-etalage", 
                 "https://nos.nl/artikel/2513359-trump-krijgt-borgsom-van-454-miljoen-dollar-niet-rond-onmogelijke-opdracht",
                 "https://www.theguardian.com/environment/2024/mar/19/air-pollution-health-report",
